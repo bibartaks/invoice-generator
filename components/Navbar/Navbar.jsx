b9dc7eb6b3dd743128ddfee19a44ebf8fd8d244f
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2024-04-12 18:07:13 +0600

    feat: add navbar

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className='py-4 border-b'>
+      <div className='container'>
+        <div>
+          <h1 className='text-lg uppercase'>Invoice Generator</h1>
+        </div>
+      </div>
+    </nav>
+  );
+}
